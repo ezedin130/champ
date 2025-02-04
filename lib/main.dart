9ab7f2dfa22c5be35e5df42372577d40cf3d7de7
ezedin130/pharma <ezedinawol130@gmail.com>
2025-02-04 15:35:38 +0300

    new file:   lib/assets/password.png 	modified:   lib/main.dart 	modified:   lib/pages/screens/login_page.dart 	modified:   lib/pages/screens/register_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:map/pages/main_screen.dart';
 import 'package:map/pages/screens/login_page.dart';
+import 'package:map/pages/screens/register_page.dart';
 import 'package:map/pages/screens/welcome_page.dart';
 
 void main() => runApp(
@@ -17,19 +19,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       locale: DevicePreview.locale(context),
+      locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      title: 'Flutter Demo',
+      title: 'Champ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      initialRoute: MainScreen.id,
       routes: {
-        'register':(context)=> LoginPage(),
-        'login':(context)=> LoginPage()
+        'register': (context) => RegisterPage(),
+        'login': (context) => LoginPage(),
+        'home': (context) => MainScreen()
       },
-      home: const WelcomePage(),
     );
   }
 }
--- lib/pages/screens/login_page.dart
+++ lib/pages/screens/login_page.dart
@@ -1,10 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:map/constant.dart';
+import 'package:map/pages/main_screen.dart';
+import 'package:map/pages/screens/register_page.dart';
 
 class LoginPage extends StatelessWidget {
   static const id = 'login';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Login to Account',
+                style: GoogleFonts.getFont(
+                  'Lato',
+                  color: const Color(0xff0d120e),
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.2,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Explore the Power Exclusives',
+                style: GoogleFonts.getFont(
+                  'Lato',
+                  color: const Color(0xff0d120e),
+                  fontSize: 14,
+                  letterSpacing: 0.2,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  //email = value;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Enter Your Email';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: inputdeco.copyWith(
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'lib/assets/email.png',
+                      color: Colors.black,
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  labelText: 'Enter Your Email Address',
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  //password = value;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Enter Your Password';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: inputdeco.copyWith(
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'lib/assets/password.png',
+                      color: Colors.black,
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  labelText: 'Enter Your Password',
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                onTap: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    Navigator.pushNamed(context, MainScreen.id);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Please fill all fields correctly")));
+                  }
+                },
+                child: Container(
+                  width: 319,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(5.0)),
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: GoogleFonts.getFont(
+                        'Lato',
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Need An Account?',
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w500, letterSpacing: 1),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, RegisterPage.id);
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: GoogleFonts.roboto(
+                          color: const Color(0XFF103DE5),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
